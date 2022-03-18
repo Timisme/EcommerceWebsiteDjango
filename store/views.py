3202220c2b63b7f4cbade7efd700bffa32217404
tim <ggg580905@gmail.com>
2022-03-18 15:55:55 +0800

    part3 finised

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import json 
+import json
+import datetime
 from .models import *
 # Create your views here.
 
@@ -92,4 +93,36 @@ def updateItem(request):
     if orderItem.quantity <= 0:
         orderItem.delete()
 
-    return JsonResponse('Item waas added', safe= False)
+    return JsonResponse('Item was added', safe= False)
+
+    '''safe: If it’s set to False, any object can be passed for serialization (otherwise only dict instances are allowed)'''
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer= customer, complete= False)
+        total = data['form']['total']
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+
+        order.save()
+
+        if total.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+    
+    else:
+        print('user is not logged in')
+    print('DATA:', request.body)
+    return JsonResponse('Payment complete!!', safe= False)
