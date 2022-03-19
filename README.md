ccbde4bf259196ee7b326a108a325c14d47cc4cb
tim <ggg580905@gmail.com>
2022-03-19 16:07:26 +0800

    payment added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EcommerceWebsiteDjango
 A ecommerce website featured with carts and payment functionality from Django 
+
+## Paypal 金流
+
+1. 使用 Paypal Sandbox 測試
+2. 創建 personal 和 business account 
+3. 創建 app, 取得 client api (有了 biz client id 交易後才知道金流給誰)
+4. 修改 paypal script amount value (由 order的總金額 取得)
---
+++ static/js/paypal.js
@@ -0,0 +1,39 @@
+// let total = '{{order.get_cart_total}}'
+
+// Render the PayPal button into #paypal-button-container
+paypal.Buttons({
+    
+    style:{
+        color: "blue",
+        shape: "rect",
+    },
+
+    // Set up the transaction
+    createOrder: function(data, actions) {
+        return actions.order.create({
+            purchase_units: [{
+                amount: {
+                    value: parseFloat(total).toFixed(2)
+                }
+            }]
+        });
+    },
+
+    // Finalize the transaction
+    onApprove: function(data, actions) {
+        return actions.order.capture().then(function(orderData) {
+            // Successful capture! For demo purposes:
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+            var transaction = orderData.purchase_units[0].payments.captures[0];
+            // alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+            submitFormData() // send data to the backend, clear the cart and redirect the user back to the home page
+            // Replace the above to show a success message within this page, e.g.
+            // const element = document.getElementById('paypal-button-container');
+            // element.innerHTML = '';
+            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+            // Or go to another URL:  actions.redirect('thank_you.html');
+        });
+    }
+
+
+}).render('#paypal-button-container');
