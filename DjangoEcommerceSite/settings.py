503763f7dfda4cf06890b1091818d64dd5f5b8e6
tim <ggg580905@gmail.com>
2022-04-09 02:56:10 +0800

    update footer & chechout page

--- DjangoEcommerceSite/settings.py
+++ DjangoEcommerceSite/settings.py
@@ -84,6 +84,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
