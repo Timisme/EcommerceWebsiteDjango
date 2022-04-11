373270a9e7c3c91a4ffc61ba8874cec49dc9ce76
tim <ggg580905@gmail.com>
2022-04-11 18:55:18 +0800

    fix nginx issue

--- django_site/entrypoint.sh
+++ django_site/entrypoint.sh
@@ -5,7 +5,7 @@ echo 'Run migration'
 python manage.py migrate --no-input
 
 echo 'Collect Static'
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 exec "$@"
 # python manage.py runserver 0.0.0.0:8080
